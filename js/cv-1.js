577b5318f4d1164be9a714720e7a9ddfa8fa3182
Michael Evans <michaelrevans@live.co.uk>
2016-02-21 00:07:52 +0100

    changed cv files to modify all tabs with col classes

--- js/cv-1.js
+++ js/cv-1.js
@@ -1,14 +1,3 @@
-/*
-$(document).ready(function() {
-  var expanded = $('button.job-btns').attr('aria-expanded');
-  if (expanded) {
-    $(this).addClass('upside-down');
-  } else {
-    $(this).removeClass('upside-down');
-  }
-});
-*/
-
 $(document).ready(function() {
   $('.job-btns').click(function() {
     $(this).toggleClass('upside-down');
