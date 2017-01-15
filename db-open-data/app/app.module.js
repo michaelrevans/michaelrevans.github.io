6a17ca1fcd9f5ef0fca64f0585c16b73e02867e0
Michael Evans <michaelrevans@live.co.uk>
2017-01-15 16:17:40 +0100

    adds nod_modules back in, updates module file and index for first test

--- db-open-data/app/app.module.js
+++ db-open-data/app/app.module.js
@@ -1,5 +1,10 @@
 var bahnApp = angular.module('BahnApp', ['ui.router']);
 
                       .config(['$urlRouterProvider', '$stateProvider', function() {
+                        $stateProvider
+                          .state('bahnApp', {
+                            url: '/',
+                            views: {}
+                          })
                         $urlRouterProvider.otherwise('/');
                       }]);
